87ac1f525b5c62c84de59e3ae9ebb03d6f100eac
AShu <ag9991323@gmail.com>
2020-02-08 18:02:38 +0530

    Modified HardCash Solution

--- HardCash.java
+++ HardCash.java
@@ -7,18 +7,18 @@ class HardCash{
         Scanner scan = new Scanner(System.in);
         int T= scan.nextInt();
         while(T--!=0){
-            int N= scan.nextInt();
+            int n= scan.nextInt();
             int K= scan.nextInt();
-            int[] array = new int[N];
-            for(int i=0;i<N;i++)
+            int[] array = new int[n];
+            for(int i=0;i<n;i++)
                 array[i]= scan.nextInt();
             BigInteger b=BigInteger.valueOf(0);
-            for(int i=0;i<N;i++){
+            for(int i=0;i<n;i++){
                 b=b.add(BigInteger.valueOf(array[i]).mod(BigInteger.valueOf(K)));
                 
                 
             } 
-            for(int i=N-1;i>=0 && (b.compareTo(BigInteger.valueOf(K))==1||b.compareTo(BigInteger.valueOf(K))==0);i--){
+            for(int i=n-1;i>=0 && (b.compareTo(BigInteger.valueOf(K))==1||b.compareTo(BigInteger.valueOf(K))==0);i--){
                 b=b.subtract(BigInteger.valueOf(K));
                   
                 }
