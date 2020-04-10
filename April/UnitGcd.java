d33ddeab52b42cc04b9ee64bbb95e3820f765122
AShu <ag9991323@gmail.com>
2020-04-10 16:04:41 +0530

    Adding solution of unitGcd problem in java

---
+++ April/UnitGcd.java
@@ -0,0 +1,33 @@
+package April;
+import java.util.*;
+public class UnitGcd {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int t= scan.nextInt();
+        while(t-- >0){
+            int n= scan.nextInt();
+            StringBuilder str = new StringBuilder();
+            if(n==1){
+                str.append(1+"\n");
+                str.append(1+" "+1+"\n");
+            }
+            else{
+                str.append(n/2+"\n");
+                if(n%2==0){
+                    for(int i=1;i<n;i+=2)
+                        str.append(2+" "+i+" "+(i+1)+"\n");
+                }
+                else{
+                    str.append(3+" "+1+" "+2+" "+3+"\n");
+                    for(int i=4;i<n;i+=2)
+                    str.append(2+" "+i+" "+(i+1)+"\n");
+
+                }
+            }
+
+            System.out.print(str);
+
+        }
+    }
+
+}
