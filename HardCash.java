e3c89374341b7f1d488f07247fc282e4db9b01cc
AShu <ag9991323@gmail.com>
2020-04-26 22:08:57 +0530

    Some changes

--- HardCash.java
+++ HardCash.java
@@ -5,6 +5,7 @@ class HardCash{
 
     public static void main(String [] args){
         Scanner scan = new Scanner(System.in);
+//
         int T= scan.nextInt();
         while(T--!=0){
             int n= scan.nextInt();
